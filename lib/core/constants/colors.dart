141634be946e2a7c6e50ea9843eacb69ccd9ccdf
EstrellaPoot <Estrella.Poot@itksquare.edu.mx>
2022-12-08 14:17:20 -0600

    chore: change color appbar

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color appBarBg = const Color(0xff383838);
-  static Color scaffoldBg = const Color(0xff2e2e2e);
+  static Color appBarBg = const Color(0xff2e2e2e);
+  static Color scaffoldBg = const Color(0xff383838);
   static Color main = const Color(0xff00d1ff);
   static Color snackbarBg = const Color(0xfff25723);
   static Color snackbarMsg = const Color(0xFFFFFFFF);
